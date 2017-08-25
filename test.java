2808e7b684b484cb7b4c665891882264e4a82695
Sakina Shivji <shivjisakina@gmail.com>
2017-08-24 23:40:24 -0400

    Done with part 1 of the course

--- test.java
+++ test.java
@@ -98,3 +98,27 @@ public class EqualityOperators {
     
 	}
 }
+
+// Generalizations
+public class Generalizations {
+	public static void main(String[] args) {
+	
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;		
+		System.out.println(epicLevel);
+
+	}
+}
+
+/* 
+Data Types are int, boolean, and char.
+Variables are used to store values.
+Whitespace helps make code easy to read for you and others.
+Comments describe code and its purpose.
+Arithmetic Operators include +, -, *, /, and %.
+Relational Operators include <, <=, >, and >=.
+Equality Operators include == and !=.
+*/
+
+// ================================================================

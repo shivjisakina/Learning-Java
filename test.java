46d0467a87486dfc876608a90c01e212f61c6dae
Sakina Shivji <shivjisakina@gmail.com>
2017-08-24 22:45:12 -0400

    got name to printin console

---
+++ test.java
@@ -0,0 +1,11 @@
+// Creating class YourName
+public class YourName {
+
+	// Making sure its a string
+	public static void main(String[] args) {
+
+		// Printing my name 
+		System.out.println("Sakina Shivji");
+
+	}
+}

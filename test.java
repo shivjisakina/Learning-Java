3b3f657683f42c5e12d53e5523fa2fa4f7407c11
Sakina Shivji <shivjisakina@gmail.com>
2017-08-24 22:56:51 -0400

    printed as many ints as I can before throwing an error, ps its 9

--- test.java
+++ test.java
@@ -9,3 +9,15 @@ public class YourName {
 
 	}
 }
+
+// Doing the same thing with integers
+public class DataTypes {
+	public static void main(String[] args) {
+
+		// Testing to see how large the number can go before I get an error (9)
+		System.out.println(552461425);
+
+		// DataTypes.java:4: error: integer number too large: 5524614251
+
+	}
+}

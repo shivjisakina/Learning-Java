c94b3214e08f4366afc185b6c17b0238910f1f01
Sakina Shivji <shivjisakina@gmail.com>
2017-09-15 18:14:46 -0400

    understanding precedense

--- test.java
+++ test.java
@@ -165,3 +165,12 @@ public class Not {
 		
 	}
 }
+
+public class Precedence {
+	public static void main(String[] args) {
+
+		boolean riddle = !( 1 < 8 || (5 > 2 && 3 < 5));
+		System.out.println(riddle); //false
+
+	}
+}
